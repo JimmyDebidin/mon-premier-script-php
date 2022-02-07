7e0359183a10ec2fa9090e6d78f9436492093b50
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-02-07 16:11:37 +0100

    challenge1_php

--- challenge1.php
+++ challenge1.php
@@ -1,1 +1,17 @@
+<?php
 
+$bookName = "Voyage avec un âne dans les Cévennes";
+$bookRead = true;
+$yearPublication = 2017;
+$fnacPrice = 5.70;
+
+echo "$bookName <br>";
+if ($bookRead) {
+    echo "je ne l'ai pas lu <br>";
+} else {
+    echo "je l'ai lu <br>";
+
+}
+    echo "$yearPublication <br>";
+    echo "$fnacPrice <br>";
+?>
